ce67ceff6c4f9ec0cc3f7902e84ba716f89e719b
Aman Kumar <cloud.aman123@gmail.com>
2026-01-21 12:26:36 +0530

    added linux practice script

---
+++ script1.sh
@@ -0,0 +1,41 @@
+#!/bin/bash 
+
+# script is made for practicing basic lunix 
+
+echo " script " 
+echo "  current linux command practice"
+
+echo " " 
+echo "1) current folder path (pwd) : "
+pwd
+
+echo " " 
+echo "2) list of files/folder (ls):"
+ls
+
+echo " " 
+echo "3) current user (whoami) :"
+whoami
+
+echo " "
+echo "4) kernel version (uname -r):"
+uname -r 
+
+echo " "
+echo "5) date and time: "
+
+date  
+
+echo " " 
+
+echo ""
+
+echo "6) show the permission in the file: "
+
+
+getfacl script1.sh
+
+echo ""
+echo " script done " 
+ 
+
